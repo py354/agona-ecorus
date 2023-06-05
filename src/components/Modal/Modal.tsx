eea737f6c0fc4ebc64832085057cf24cfcfe7b9c
Danis Kirasirov <dbgbbu@gmail.com>
2023-06-05 07:06:54 +0300

    ESLint и Prettier победили меня

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -7,17 +7,17 @@ import { RootState } from "../../store/store";
 import { destroyModal } from "../../store/Modal";
 
 export function Modal() {
-  const { currentForm } = useSelector((state: RootState) => state.modal);
-  const dispatch = useDispatch();
+    const { currentForm } = useSelector((state: RootState) => state.modal);
+    const dispatch = useDispatch();
 
-  return (
-    <ReactModal
-      isOpen={currentForm !== null}
-      className={styles.modalWrapper}
-      overlayClassName={styles.overlay}
-      onRequestClose={() => dispatch(destroyModal())}
-    >
-      {currentForm}
-    </ReactModal>
-  );
+    return (
+		<ReactModal
+			isOpen={currentForm !== null}
+			className={styles.modalWrapper}
+			overlayClassName={styles.overlay}
+			onRequestClose={() => dispatch(destroyModal())}
+		>
+            {currentForm}
+        </ReactModal>
+	);
 }
--- src/store/store.ts
+++ src/store/store.ts
@@ -4,14 +4,14 @@ import { profileSlice } from "./Profile";
 import { modalSlice } from "./Modal";
 
 export const rootStore = configureStore({
-  devTools: true,
-  reducer: {
-    [modalSlice.name]: modalSlice.reducer,
-    [profileSlice.name]: profileSlice.reducer,
-    [AuthApi.reducerPath]: AuthApi.reducer,
-  },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat([AuthApi.middleware]),
+    devTools: true,
+	reducer: {
+		[modalSlice.name]: modalSlice.reducer,
+		[profileSlice.name]: profileSlice.reducer,
+        [AuthApi.reducerPath]: AuthApi.reducer,
+	},
+    middleware: (getDefaultMiddleware) =>
+		getDefaultMiddleware().concat([AuthApi.middleware]),
 });
 
 export type RootState = ReturnType<typeof rootStore.getState>;
